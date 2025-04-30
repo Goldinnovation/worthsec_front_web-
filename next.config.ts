c2f7795e240db404a05d84178cfd659dfc71df92
Emmanuel Neuenhaus <emanuelneuenhaus@hotmail.com>
2025-04-30 20:37:21 +0200

    added an dishable for xpowerd http header and clieckjacking

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   output: "standalone"
+   output: "standalone",
+   poweredByHeader: false,
+   async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes 
+        headers: [
+          {
+            key: 'Content-Security-Policy',  //avoid iframe embedding - clickjacking
+            value: "frame-ancestors 'none';", //ensure script protections
+          },
+      
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
